52ed5b3b07df9495c6b3d5bcb5f0a34db5f31ff1
Michael Hurley <Michael.Hurley@abcgrp.com>
2018-11-06 15:11:12 -0500

    Added build

---
+++ precache-manifest.019cd38dbc53cbfd187f5c5aafb66e09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "80742c64ea07c0d52337",
+    "url": "/static/js/main.80742c64.chunk.js"
+  },
+  {
+    "revision": "3ebf749c3273b75d9ff8",
+    "url": "/static/js/1.3ebf749c.chunk.js"
+  },
+  {
+    "revision": "3ebf749c3273b75d9ff8",
+    "url": "/static/css/1.34c55c23.chunk.css"
+  },
+  {
+    "revision": "b3f0646f9e65086dce69bb36f94670fa",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/quick-nutrient-log/precache-manifest.612712611fddb3d552ed2cde4348be12.js"
+  "/precache-manifest.019cd38dbc53cbfd187f5c5aafb66e09.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/quick-nutrient-log/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
